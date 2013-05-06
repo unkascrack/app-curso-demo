beb6f8396231f6ad7f2be05e8b39c1cd8a66201b
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@551b1bf2-88f1-4463-f504-92a2c45cfc8e>
2013-05-06 21:44:41 +0000

    Añadido método de test y constructor

--- trunk/src/test/java/es/curso/demo/mapper/CursoMapperTest.java
+++ trunk/src/test/java/es/curso/demo/mapper/CursoMapperTest.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import es.curso.demo.model.Curso;
+import es.curso.demo.model.Nivel;
+import es.curso.demo.model.Tutor;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring-config-test.xml" })
 public class CursoMapperTest {
@@ -18,5 +22,17 @@ public class CursoMapperTest {
     @Test
     public void testCursoMapperNotNull() {
         Assert.assertNotNull(cursoMapper);
+    }
+
+    @Test
+    public void testInsert() {
+        final Curso curso = new Curso();
+        curso.setActivo(false);
+        curso.setTitulo("Mi Curso");
+        curso.setNivel(Nivel.BASICO);
+        curso.setHoras(10);
+        curso.setTutor(new Tutor(1l));
+        cursoMapper.insert(curso);
+        Assert.assertNotNull(curso.getId());
     }
 }
