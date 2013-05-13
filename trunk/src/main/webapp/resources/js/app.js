a3445548301831b94ff1d21e2919a6fce331b798
carlos.alonso.gonzalez@gmail.com <carlos.alonso.gonzalez@gmail.com@551b1bf2-88f1-4463-f504-92a2c45cfc8e>
2013-05-13 21:42:26 +0000

    Refactoring del modificadicador del camop de texto TextField para subida de documentos: upload.

--- trunk/src/main/webapp/resources/js/app.js
+++ trunk/src/main/webapp/resources/js/app.js
@@ -20,3 +20,22 @@ $(document).ready(function(){
 Ember.Select.reopen({
 	attributeBindings: ['required']
 });
+
+App.UploadFileView = Ember.TextField.extend({
+	type: 'file',
+    attributeBindings: ['name'],
+    change: function(evt) {
+		var self = this;
+		var input = evt.target;
+		if (input.files && input.files[0]) {
+        	var reader = new FileReader();
+        	reader.onload = function(e) {
+				var fileToUpload = e.srcElement.result;
+				var name = self.get('name');
+				self.get('controller').set(name, fileToUpload);
+				self.get('controller').get('content').set(name, input.files[0].name);
+        	};
+        	reader.readAsDataURL(input.files[0]);
+      	}
+	}
+});
