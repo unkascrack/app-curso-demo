913aa5061105436fba73b42aba3b653188bd51b8
carlos.alonso.gonzalez <carlos.alonso.gonzalez@gmail.com>
2013-05-13 20:48:27 +0000

    Revisión de la navegación

--- src/main/webapp/resources/js/routes.js
+++ src/main/webapp/resources/js/routes.js
@@ -12,6 +12,7 @@ App.IndexRoute = Ember.Route.extend({
 
 App.CursosRoute = Ember.Route.extend({
 	model: function(params) {
+		this.get('store.defaultTransaction').rollback();
         return App.Curso.find({ activo: true });
     }
 });
