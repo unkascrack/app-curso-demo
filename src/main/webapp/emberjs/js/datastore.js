f4503dff38881250fca287790345a6fd2a8f179a
carlos.alonso.gonzalez <carlos.alonso.gonzalez@gmail.com>
2013-05-21 10:23:56 +0000

    Revisión del proyecto para poder distintos frameworsk javascript mvc

--- src/main/webapp/emberjs/js/datastore.js
+++ src/main/webapp/emberjs/js/datastore.js
@@ -1,8 +1,8 @@
 App.Store = DS.Store.extend({
 	revision: 12,
 	adapter: DS.RESTAdapter.extend({
-		url: 'http://localhost:8080/',
-		namespace: 'curso-demo/rest',
+		url: getURL(),
+		namespace: 'rest',
 		bulkCommit: false,
 		serializer: DS.RESTSerializer.extend({
 			init: function() {
@@ -17,3 +17,10 @@ DS.RESTAdapter.configure('plurals', {
 	tutor: 'tutores',
 	curso: 'cursos'
 });
+
+function getURL() {
+	var protocol = location.protocol + '//';
+	var hostname = location.hostname + ':' + location.port + '/';
+	var context = location.pathname.substr(1, location.pathname.substr(1).indexOf('/')) + '/';
+	return protocol + hostname + context;
+}
