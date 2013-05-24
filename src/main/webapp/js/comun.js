edaaf9bf63bbffa811c06357e289cc1601da19e2
carlos.alonso.gonzalez <carlos.alonso.gonzalez@gmail.com>
2013-05-24 12:18:35 +0000

    Revisión interface angulrjs

--- src/main/webapp/js/comun.js
+++ src/main/webapp/js/comun.js
@@ -4,3 +4,10 @@ function getURL() {
 	var context = location.pathname.substr(1, location.pathname.substr(1).indexOf('/')) + '/';
 	return protocol + hostname + context;
 }
+
+function getURLEscaped() {
+	var protocol = location.protocol + '//';
+	var hostname = location.hostname + '\\:' + location.port + '/';
+	var context = location.pathname.substr(1, location.pathname.substr(1).indexOf('/')) + '/';
+	return protocol + hostname + context;
+}
